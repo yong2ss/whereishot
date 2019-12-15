1798a005f5e3a1c5d5794b0fd028a89478752a63
korea92co <jr_good_si@naver.com>
2019-12-15 19:42:28 +0900

    lombok commit

---
+++ src/main/java/com/yong2ss/whereishot/wih/Application.java
@@ -0,0 +1,11 @@
+package com.yong2ss.whereishot.wih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
